391958a9e144a070bc57ee82e759286cb7e64322
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-12-07 16:50:00 +0000

    Prototyping functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+//! # The Network Manager Library
+//!
+//! The Network Manager Library provides the essential
+//! functionality for configuring Network Manager from Rust.
+
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+pub mod general;
+pub mod wifi;
+pub mod service;
+pub mod connection;
+pub mod device;
