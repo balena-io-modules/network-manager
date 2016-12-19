e9f2a280416ee36ff98ea2ff46ef948fed122929
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-12-19 11:56:33 +0000

    Implementing function to stop the Network Manager service

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -38,14 +38,6 @@ pub fn enable(to: i32) -> Result<ServiceState, String> {
     Ok(s)
 }
 
-
-
-// bus.getInterfaceAsync(SERVICE, MANAGER_OBJECT, MANAGER_INTERFACE)
-//     .then (manager) ->
-//         manager.StartUnitAsync(unit, mode)
-//     .then ->
-//         waitUntilState(unit, 'active')
-
 /// Disables the Network Manager service.
 ///
 /// # Examples
@@ -54,15 +46,30 @@ pub fn enable(to: i32) -> Result<ServiceState, String> {
 /// let state = network_manager::service::disable(10).unwrap();
 /// println!("{:?}", state);
 /// ```
-pub fn disable(t: i32) -> Result<ServiceState, String> {
-    // Disable service
+pub fn disable(to: i32) -> Result<ServiceState, String> {
+    let c = Connection::get_private(BusType::System).unwrap();
 
-    if t != 0 {
-        // Wait until service has stopped or
-        // until the time has elapsed
+    let mut m = Message::new_method_call("org.freedesktop.systemd1",
+                                         "/org/freedesktop/systemd1",
+                                         "org.freedesktop.systemd1.Manager",
+                                         "StopUnit")
+        .unwrap();
+    m.append_items(&["NetworkManager.service".into(), "fail".into()]);
+    c.send_with_reply_and_block(m, 2000).unwrap();
+
+    let mut s = state().unwrap();
+    let mut t = 0;
+    while t < to {
+        thread::sleep(time::Duration::from_secs(1));
+        t = t + 1;
+
+        s = state().unwrap();
+        if s == ServiceState::Inactive {
+            break;
+        }
     }
 
-    Ok(ServiceState::Inactive)
+    Ok(s)
 }
 
 /// Gets the state of the Network Manager service.
